d2399960f98b92bc184829239d71dba3608da446
Will Campbell <wibb1@comcast.net>
2020-12-06 14:46:48 -0500

    Implemented edit and update functionality

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -13,9 +13,25 @@ class PortfoliosController < ApplicationController
 
     respond_to do |format|
       if @portfolio_item.save
-        format.html { redirect_to @portfolio_item, notice: "Your portfolio item is now live." }
+        format.html { redirect_to portfolios_path, notice: "Your portfolio item is now live." }
       else
         format.html { render :new }
+      end
+    end
+  end
+
+  def edit
+    @portfolio_item = Portfolio.find(params[:id])
+  end
+
+  def update
+    @portfolio_item = Portfolio.find(params[:id])
+
+    respond_to do |format|
+      if @portfolio_item.update(params.require(:portfolio).permit(:title, :subtitle, :body, :main_image, :thumb_image))
+        format.html { redirect_to portfolios_path, notice: "Your portfolio item has been updated." }
+      else
+        format.html { render :edit }
       end
     end
   end
