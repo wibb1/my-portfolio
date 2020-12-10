ccdbc653dc3507d36a4027d0001f314a77a63b93
Will Campbell <wibb1@comcast.net>
2020-12-10 00:14:54 -0500

    Added defaults for portfolios

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -6,5 +6,12 @@ class Portfolio < ApplicationRecord
   end
 
   scope :rails, -> { where(:subtitle => 'Ruby on Rails') }
+ 
+  after_initialize :set_defaults 
+  
+  def set_defaults
+    self.main_image ||="http://placehold.it/600x400"
+    self.thumb_image ||="http://placehold.it/350x200"
+  end
 
 end
