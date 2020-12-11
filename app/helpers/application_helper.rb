757cb7e38284b9efe01147c22b9368092caa10c6
Will Campbell <wibb1@comcast.net>
2020-12-11 14:08:13 -0500

    Added view helpers

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,15 @@
 module ApplicationHelper
+  def application_helper_login
+    if current_user.is_a?(User)
+      link_to "Logout", destroy_user_session_path, method: :delete
+    else
+      (link_to "Register", new_user_registration_path) +
+      "<br>".html_safe +
+      (link_to "Login", new_user_session_path)
+    end
+  end
+
+  def application_helper_message
+    session[:source].present? ? " from #{session[:source]}!" : "!"
+  end
 end
