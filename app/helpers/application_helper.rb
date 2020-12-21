666c994f7d0376d2b376a2d4a47ac83bb529d12f
Will Campbell <wibb1@comcast.net>
2020-12-21 00:07:30 -0500

    Refactored active status and navigation

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,28 @@
 module ApplicationHelper
-  def application_helper_login style = ''
+def guest_login_items
+  [
+    {
+      url: new_user_registration_path,
+      title: "Register"
+    },
+    {
+      url: new_user_session_path,
+      title: "Login"
+    }
+  ]
+end
+
+  def application_helper_login style = '', tag_type 
     if current_user.is_a?(GuestUser)
-      (link_to "Register", new_user_registration_path, class: style) +
-      " ".html_safe +
-      (link_to "Login", new_user_session_path, class: style)
+      login_links = ''
+      guest_login_items.each do |item|
+        login_links << "<#{tag_type}><a href='#{item[:url]}' class='#{style} #{active? item[:url]}'>#{item[:title]}</a></#{tag_type}>"
+      end
+  
+      login_links.html_safe
+
+      ##(link_to "Register", new_user_registration_path, class: style) 
+      ##(link_to "Login", new_user_session_path, class: style)
     else
       link_to "Logout", destroy_user_session_path, method: :delete, class: style
     end
@@ -18,5 +37,43 @@ module ApplicationHelper
 
   def application_copyright_generator
     Wibb1ViewTool::Renderer.copyright 'Will Campbell', 'All rights reserved'
+  end
+
+  def nav_items
+    [
+      {
+        url: root_path,
+        title: 'Home'
+      },
+      {
+        url: about_me_path,
+        title: 'About Me'
+      },
+      {
+        url: contact_path,
+        title: 'Contact'
+      },
+      {
+        url: blogs_path,
+        title: 'Blog'
+      },
+      {
+        url: portfolios_path,
+        title: 'Portfolio'
+      }
+    ]
+  end
+
+  def application_helper_nav style, tag_type
+    nav_links = ''
+    nav_items.each do |item|
+      nav_links << "<#{tag_type}><a href='#{item[:url]}' class='#{style} #{active? item[:url]}'>#{item[:title]}</a></#{tag_type}>"
+    end
+
+    nav_links.html_safe
+  end
+
+  def active? path
+    "active" if current_page? path
   end
 end
