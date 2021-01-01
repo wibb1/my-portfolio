f9f2d33159b64535aea7bfc46d105f5efdd755a0
Will Campbell <wibb1@comcast.net>
2020-12-31 20:44:44 -0500

    refactored seeds file to incorporate default site_admin while in development and carrierwave changes

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,6 +4,8 @@
   )  
 end
 
+puts "3 topics created"
+
 10.times do |blog|
   Blog.create!(
     title: "My Blog Post #{blog}",
@@ -11,6 +13,8 @@ end
     topic_id: Topic.last.id
   )
 end
+
+puts "10 blog posts created"
 
 5.times do |skill|
   Skill.create!(
@@ -23,18 +27,24 @@ end
   title: "Portfolio Title #{item}",
   subtitle: "Ruby on Rails",
   body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-  main_image: "https://via.placeholder.com/600x400",
-  thumb_image: "https://via.placeholder.com/350x200")
+  main_image: Rails.root.join("app/assets/images/default_main_image.png").open,
+  thumb_image: Rails.root.join("app/assets/images/default_thumb_image.png").open
+  )
 end
+
+puts "8 portfolio items created with Ruby on Rails subtitle"
 
 1.times do |item|
   Portfolio.create!(
   title: "Portfolio Title #{item}",
   subtitle: "Angular",
   body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-  main_image: "https://via.placeholder.com/600x400",
-  thumb_image: "https://via.placeholder.com/350x200")
+  main_image: Rails.root.join("app/assets/images/default_main_image.png").open,
+  thumb_image: Rails.root.join("app/assets/images/default_thumb_image.png").open
+  )
 end
+
+puts "1 portfolio item created with Angular subtitle"
 
 3.times do |technology|
   Portfolio.last.technologies.create!(
@@ -43,3 +53,12 @@ end
 end
 
 puts "3 technologies created"
+
+User.create!(
+  email: "thumb@aol.com",
+  password: "123456",
+  name: "Tom Thumb",
+  roles: "site_admin"
+)
+
+puts "default admin user created"
