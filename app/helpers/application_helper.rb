168f1768c51afe30b5ec95c8a49892e597907f91
Will Campbell <wibb1@comcast.net>
2021-01-02 18:32:34 -0500

    Refactored the error/alert/notice messages into a application_helper method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -76,4 +76,16 @@ end
   def active? path
     "active" if current_page? path
   end
+
+  def alerts
+    alert = (flash[:alert] || flash[:error] || flash[:notice])
+    if alert 
+      alert_generator alert
+    end
+  end
+
+  def alert_generator msg 
+    js add_gritter(msg, title: "Will Campbell's Portfolio", sticky: false)
+  end
+
 end
