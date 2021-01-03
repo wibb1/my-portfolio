04c11fdf8c36785ff5599546ad765196c6196e72
Will Campbell <wibb1@comcast.net>
2021-01-03 10:28:58 -0500

    created comment broadcast, configured websocket route, and updated the comment model with a callback

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -3,4 +3,6 @@ class Comment < ApplicationRecord
   belongs_to :blog
 
   validates :content, presence: true, length: { minimum: 4, maximum: 1000 }
+
+  after_create_commit { CommentBrodcastJob.perfom_later(self) }
 end
