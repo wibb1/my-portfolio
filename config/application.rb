3bf209217b252a30acb686188a348b4cad2d2c4a
Will Campbell <wibb1@comcast.net>
2021-01-02 23:20:06 -0500

    Added twitter gem and initial integration with Twitter API

--- config/application.rb
+++ config/application.rb
@@ -23,5 +23,6 @@ module DevcampPortfolio
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
     config.generators.system_tests = nil
+    config.eager_load_paths << "#{Rails.root}/lib"
   end
 end
