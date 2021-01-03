3e0d806ab39760399180c820c02ef1093515b8e2
Will Campbell <wibb1@comcast.net>
2021-01-03 00:21:50 -0500

    Added twitter parser in pages helper

--- lib/social_tool.rb
+++ lib/social_tool.rb
@@ -7,7 +7,7 @@ module SocialTool
       config.access_token_secret = ENV.fetch("TWITTER_ACCESS_SECRET")
     end
 
-    client.search("#rubyonrails", lang: "en", result_type: "recent").take(10).collect do |tweet|
+    client.search("#rubyonrails", lang: "en", result_type: "recent").take(6).collect do |tweet|
       "#{tweet.user.screen_name}: #{tweet.text}"
     end
   end
