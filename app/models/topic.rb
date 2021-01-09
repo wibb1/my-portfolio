50791fc711f268db14801feded16d40518efd9fa
Will Campbell <wibb1@comcast.net>
2021-01-08 22:48:28 -0500

    Add function to check blog count before destroy

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -2,4 +2,9 @@ class Topic < ApplicationRecord
   validates_presence_of :title
 
   has_many :blogs
+
+  def destroy
+    raise "Cannot delete topic with blogs" unless blogs.count == 0
+    super
+  end
 end
