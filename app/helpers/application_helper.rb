80cea6c59c005f320dde08d73324e8ff3eb3ad78
Will Campbell <wibb1@comcast.net>
2021-01-13 18:48:17 -0500

    Changed duration for notice messages in gritter

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -89,6 +89,6 @@ end
   end
 
   def alert_generator msg 
-    js add_gritter(msg, title: "Will Campbell's Portfolio", sticky: false, time: 2000)
+    js add_gritter(msg, title: "Will Campbell's Portfolio", sticky: false, time: 1500)
   end
 end
