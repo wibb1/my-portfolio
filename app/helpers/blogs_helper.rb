e21a6f5a694bcf62b2df23b07b46ec4c1e94fe9a
Will Campbell <wibb1@comcast.net>
2021-01-13 15:13:14 -0500

    Moved coderay and redcarpet imp to blogs_helper

--- app/helpers/blogs_helper.rb
+++ app/helpers/blogs_helper.rb
@@ -34,4 +34,34 @@ module BlogsHelper
     end
     link_to fa_icon(icon, title: "Current status: #{status.titleize}\nClick to change"), toggle_status_blog_path(blog), class: "btn btn-#{color} fa-2x"
   end
+
+  def markdown(text)
+    transformed = CodeRayImp.new(
+      filter_html: true, 
+      hard_wrap: true, 
+      prettify: true, 
+      link_attributes: true
+      )
+    
+    markdown_options = {
+      no_intra_emphasis: true,
+      fenced_code_blocks: true,
+      disable_indented_code_blocks: true,
+      superscript: true,
+      underline: true,
+      highlight: true,
+      autolink: true,
+      lax_html_blocks: true
+    }
+
+    markdown_text = Redcarpet::Markdown.new(Redcarpet::Render::HTML, markdown_options)
+    markdown_text.render(text).html_safe
+  end
+
+  class CodeRayImp < Redcarpet::Render::HTML
+    def code_block(code, language)
+      language ||= :plaintext
+      CodeRay.scan(code, language).div
+    end
+  end
 end
