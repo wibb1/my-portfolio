419fd18ae98fd12526a9783b0ee791636d56b3e1
Will Campbell <wibb1@comcast.net>
2021-01-15 19:26:50 -0500

    Changed show page title

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -18,7 +18,7 @@ class BlogsController < ApplicationController
     if logged_in?(:site_admin) || @blog.published?
       @blog = Blog.includes(:comments).friendly.find(params[:id]) 
       @comment = Comment.new
-      @page_title = @blog.title
+      @page_title = "Blog Detail Page"
       @seo_keywords += @blog.body
     else
       redirect_to blogs_path, notice: "You are not authorized to access this page"
