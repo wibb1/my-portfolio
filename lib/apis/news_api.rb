f1543433c9a5010ce6bedc7c6541915a4507c86f
Will Campbell <wibb1@comcast.net>
2021-01-24 17:03:30 -0500

    Improved results for news_api and errors

--- lib/apis/news_api.rb
+++ lib/apis/news_api.rb
@@ -12,8 +12,10 @@ module Apis
         end
 
         def user_search(search_terms, start_date, sort_by)
-      
-          url = "q=#{search_terms}&from=#{start_date}&sortBy=#{sort_by}&language=en&page=2"
+          from_date = (start_date-7).iso8601
+          to_date = start_date.iso8601
+
+          url = "q=#{search_terms}&from=#{from_date}&to=#{to_date}&sortBy=#{sort_by}&language=en&page=2"
       
           response = Faraday.get("http://newsapi.org/v2/everything?#{url}") do |req|
             req.headers["x-api-key"] = @api_key
@@ -21,18 +23,19 @@ module Apis
           
           parsed_response = JSON.parse(response.body)
           parsed_results = parsed_response['articles']
+          response_code = response.env.status
           
-          return parsed_results if response_successful?(response.env.status)
-          
-          raise StandardError.new "Code: #{response.env.status}, Response: #{parsed_response['message']}"
+          return parsed_results if response_successful?(response_code)
+          begin
+            error_message = "Code: #{response_code}, Response: #{parsed_response['message']}"
+            raise StandardError.new "#{error_message}"
+          rescue
+            redirect_to blogs_path, flash: { error: error_message }
+          end
         end
 
-        def response_successful?(response)
-          response == 200
-        end
-
-        def api_requests_quota_reached?
-          response.body.match?('API rate limit exceeded')
+        def response_successful?(response_code)
+          response_code == 200
         end
       end
     end
