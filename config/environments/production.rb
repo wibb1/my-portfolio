ef67428ea7a8473f55515ff35e0adad8dc0cf9cf
Will Campbell <wibb1@comcast.net>
2021-01-28 12:17:30 -0500

    Add production calls for action cable

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,4 +91,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.action_cable.allowed_request_origins = ['https://wkc-my-portfolio-app.herokuapp.com', 'http://wkc-my-portfolio-app.herokuapp.com']
+  config.action_cable.url = "wss://wkc-my-portfolio-app.herokuapp.com/cable"
 end
